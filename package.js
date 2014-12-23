e4037794c90b2cde4e07dc5a7a9e92393f72ca56
james <jamesluck@justgiving.com>
2014-12-23 15:18:15 +0000

    Upgrading azure-active-directory package

--- package.js
+++ package.js
@@ -1,15 +1,17 @@
 Package.describe({
     summary: "Login service for Azure Active Directory",
-    version: "0.1.0"
+    version: "0.1.1",
+    name: "wiseguyeh:accounts-azure-active-directory",
+    git: "https://github.com/djluck/accounts-azure-active-directory"
 });
 
 Package.onUse(function(api) {
-    api.use(['underscore', 'random']);
-    api.use('accounts-base', ['client', 'server']);
+    api.use(['underscore@1.0.1', 'random@1.0.1']);
+    api.use('accounts-base@1.1.2', ['client', 'server']);
 // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
-    api.use('accounts-oauth', ['client', 'server']);
-    api.use('azure-active-directory', ['client', 'server']);
+    api.use('accounts-oauth@1.1.2', ['client', 'server']);
+    api.use('wiseguyeh:azure-active-directory@0.1.2', ['client', 'server']);
 
     api.addFiles("azure_ad.js");
 });
