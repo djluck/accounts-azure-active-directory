92d251045874a1c4d8c5b519cae6868ea697f748
James Luck <djluck20@googlemail.com>
2015-04-07 16:20:32 +0100

    Updated package version and azure-active-directory dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Login service for Azure Active Directory",
-    version: "0.3.0",
+    version: "0.3.1",
     name: "wiseguyeh:accounts-azure-active-directory",
     git: "https://github.com/djluck/accounts-azure-active-directory"
 });
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
     api.use('accounts-oauth@1.1.3', ['client', 'server']);
-    api.use('wiseguyeh:azure-active-directory@0.3.0', ['client', 'server']);
+    api.use('wiseguyeh:azure-active-directory@0.3.1', ['client', 'server']);
 
     api.addFiles('azure_ad_login_buttons.css', 'client');
 
